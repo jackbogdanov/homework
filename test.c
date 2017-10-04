262496f5c83066572b3ec42b4b4415c79ebb7d3d
jackbogdanov <gekabog@gmail.com>
2017-10-04 21:00:20 +0300

    Первое дз, проверка на индианность.

---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+union Data{
+	int i;
+	char c[sizeof(int)];
+}data;
+
+int main(){
+	data.i = 1000;
+	printf( "Memory : %d %d %d %d \n ", data.c[0], data.c[1], data.c[2], data.c[3]);
+	return 0;
+}
