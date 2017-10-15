ae188933dcd7f5202ce759d684cdbc71813407f6
jackbogdanov <gekabog@gmail.com>
2017-10-15 22:56:18 +0300

    Исправил ошибки в тестах.

--- bits.c
+++ bits.c
@@ -38,41 +38,42 @@ int sign(int x){
 
 int getByte(int x, int n){
     int msk = 255;
-    while (n){
-        msk = msk << 8;
-        n += ~0;
-    }
+    int move = n << 3;
 
-    return x & msk;
+    return (x >> move) & msk;
 }
 
 int logicalShift(int x, int n){
     int msk = ~((1 << 31) >> (n + ~0));
-    return (x >> n) & msk;
+
+    return ((x >> n) & msk & n) | x;
 }
 
 int addOK(int x, int y){
     int check = ((x >> 31) + (y >> 31)) + 1;
     int sum = x + y;
     int sumDetector = ((sum >> 31) | (sum >> 31) + 1);
-    int controlSum = check + (sumDetector + ~0);
-    return !check | !(controlSum ^ 1) | !(controlSum) ;
+
+    int controlSum = check + sumDetector;
+
+    return !check | ~!controlSum + 2;
 }
 
 int bang(int x){
     int movedX = (~x >> 1) & ~(1 << 31);
     int r = (movedX + 1) >> 31;
 
-
-    return (3 + ~(r + 2)) & x ^ 1;
+    return (3 + ~(r + 2)) & x + r;
 }
+
 
 int conditional(int x, int y, int z){
     return (y & (!x + ~0)) | (z & ~(!x + ~0));
 }
 
 int isPower2(int x){
-    return !((x & (~x + 1)) ^ x) & ~!x;
+    int msk = 1 << 31;
+    return (!((x & (~x + 1)) ^ x) & ~!x) & ~!(x ^ msk);
 }
 
 int main() {
@@ -81,12 +82,12 @@ int main() {
     printf("thirdBits %d\n", thirdBits());
     printf("fitsBits %d\n", fitsBits(-4, 3));
     printf("sign %d\n", sign(23));
-    printf("getByte %x\n", getByte(0x12345678, 1));
-    printf("logicalShift %x\n", logicalShift(0x87654321, 4));
-    printf("addOK %d\n", addOK(0x7fffffff, 0x7fffffff));
-    printf("bang %d\n", bang(-23));
+    printf("getByte %x\n", getByte(0x12345678, 0));
+    printf("logicalShift %x\n", logicalShift(0x80000000, 1));
+    printf("addOK %d\n", addOK(0x80000000, 0x70000000));
+    printf("bang %d\n", bang(0));
     printf("conditional %d\n", conditional(0, 34, 5));
-    printf("isPower2 %d\n", isPower2(128));
+    printf("isPower2 %d\n", isPower2(0x80000000));
 
     return 0;
 }
