d599eedc3682214982c279ff4994b93d0b362fa1
Jack <jackbogdanov@github.com>
2017-12-12 23:47:51 +0300

    Some fixes.

--- fifthTask.c
+++ fifthTask.c
@@ -5,28 +5,39 @@ int main() {
 
     FILE *file1 = fopen("test.txt", "r");
     FILE *file2 = fopen("test.txt", "r");
-    char str1[255], str2[255];
 
-    while (fgets(str1, 255, file1)){
-        int flag = 0;
-        while (fgets(str2, 255, file2)){
-            printf("%s %s %d\n", str1, str2, strcmp(str1, str2));
+    if (file1 != NULL && file2 != NULL) {
 
-            if (!strcmp(str1, str2)){
-                flag++;
+        char str1[255], str2[255];
+
+        while (fgets(str1, 255, file1)) {
+            int flag = 0;
+            
+            while (fgets(str2, 255, file2)) {
+                int res = strcmp(str1, str2);
+                
+                printf("%s %s %d\n", str1, str2, res);
+
+                if (!res) {
+                    flag++;
+                }
+            }
+
+            if (flag == 1) {
+                printf(" %s", str1);
+                break;
+            } else {
+                fclose(file2);
+                file2 = fopen("test.txt", "r");
             }
         }
 
-        if (flag == 1){
-            printf(" %s", str1);
-            break;
-        } else {
-            fclose(file2);
-            file2 = fopen("test.txt", "r");
-        }
+        fclose(file1);
+        fclose(file2);
+    } else {
+        printf("Не удалось открыть файл:(");
     }
 
-    fclose(file1);
-    fclose(file2);
+
     return 0;
 }
