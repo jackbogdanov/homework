a74b2a54b748519d8f2a03967b13483a04db3973
Jack <jackbogdanov@github.com>
2017-12-12 23:40:55 +0300

    Some fixes.

--- thirdTask.c
+++ thirdTask.c
@@ -5,23 +5,29 @@ int main() {
     FILE *file = fopen("/home/evgenij/test.txt", "r");
     char str[255];
 
-    while (fgets(str, 255, file)){
+    if (file != NULL) {
+        while (fgets(str, 255, file)) {
 
-        int i = 0;
+            int i = 0;
+            int len = (int) strlen(str);
 
-        while (i < strlen(str)- 1){
-            if (str[i] == '/' && str[i + 1] == '/'){
-                while (str[i] != '\n'){
-                    printf("%c", str[i]);
-                    i ++;
+            while (i < len- 1) {
+                if (str[i] == '/' && str[i + 1] == '/') {
+                    while (str[i] != '\n') {
+                        printf("%c", str[i]);
+                        i ++;
+                    }
+                    printf("\n");
                 }
-                printf("\n");
+                i++;
             }
-            i++;
         }
+
+        fclose(file);
+
+    } else {
+        printf("Не удалось открыть файл:(");
     }
-
-    fclose(file);
-
+    
     return 0;
 }
